60c24c2a2298f08e4b6252222a633b5cea2b7501
Paul Dulce <paul.dulce2107@gmail.com>
2026-04-30 19:01:29 +0200

    test mit arrays, aber mit Fehlern

---
+++ board/board.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+brett = np.zeros(64)
+
+
+class Board:
+
+    
+    def __init__(self):
+        self.brett = 
+        self.initial_position()
+        
+
+    def initial_position(self):
+        
