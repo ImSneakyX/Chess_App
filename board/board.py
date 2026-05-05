19ad5e9706be7274949a01150acbd44cc61e1f93
Paul Dulce <paul.dulce2107@gmail.com>
2026-05-05 15:47:58 +0200

    display Methode erstellt

--- board/board.py
+++ board/board.py
@@ -39,52 +39,79 @@ class Board:
     def __init__(self):
         self.brett = np.zeros(64, 'object')
         self.brett = self.brett.reshape(8,8)
-        self.pawn = Pawn('w')
-        self.knight = Knight('w')
-        self.bishop = Bishop('w')
-        self.rook = Rook('w')
-        self.queen = Queen('w')
-        self.king = King('w')
-        self.start_position('w')
-        self.color()
+
+        #weiße Figuren
+        self.pawn_w = Pawn('w')
+        self.knight_w = Knight('w')
+        self.bishop_w = Bishop('w')
+        self.rook_w = Rook('w')
+        self.queen_w = Queen('w')
+        self.king_w = King('w')
+
+        #Schwarze Figuren
+        self.pawn_b = Pawn('b')
+        self.knight_b = Knight('b')
+        self.bishop_b = Bishop('b')
+        self.rook_b = Rook('b')
+        self.queen_b = Queen('b')
+        self.king_b = King('b')
+        
+        self.start_position()
+
+        
     def start_position(self):
         self.start = self.brett
 
         #Bauern
-        self.start[1,:] = self.pawn
-        self.start[-2,:] = self.pawn
+        self.start[1,:] = self.pawn_b
+        self.start[-2,:] = self.pawn_w
         
         #Türme
 
-        row = [0,-1, 0,-1]
-        col = [0, 0, -1, -1]
+        row = [0,0]
+        col = [0, -1]
+        self.start[row, col] = self.rook_b
 
-        self.start[row, col] = self.rook
+        row = [-1,-1]
+        col = [0, -1]
+        self.start[row, col] = self.rook_w
 
         #Springer
-        col = [1, 1, -2, -2]
-        row = [0,-1, 0, -1]
-        self.start[row, col] = self.knight
+
+        row = [0,0]
+        col = [1, -2]
+        self.start[row, col] = self.knight_b
+
+        row = [-1, -1]
+        col = [1, -2]
+        self.start[row, col] = self.knight_w
 
         #Läufer
-        col = [2, 2, -3, -3]
-        row = [0, -1, 0, -1]
-        self.start[row, col] = self.bishop
+        row = [0,0]
+        col = [2, -3]
+        self.start[row, col] = self.bishop_b
+
+        row = [-1, -1]
+        col = [2, -3]
+        self.start[row, col] = self.bishop_w
 
         #Dame
-        row = [0, -1]
-        col = [3, 3]
-        self.start[row, col] = self.queen
+        self.start[0, 3] = self.queen_b
+        self.start[-1,3] = self.queen_w
 
         #König
-        row = [0, -1]
-        col = [4,4]
-        self.start[row, col] = self.king
+        self.start[0, 4] = self.king_b
+        self.start[-1,4] = self.king_w
 
+    def display(self, attribute):
+        for rows, i in enumerate(self.start):
+            for cols, j in enumerate(i):
+                if self.start[rows, cols] == 0:
+                    print(0, end=' ')
+                else:
+                    value = getattr(self.start[rows, cols], attribute)
+                    print(f"{value}", end = ' ')
+            print()
 
-    def color(self):
-        self.color = self.brett
-        
 brett = Board()
-print(brett.start)
-        
+brett.display('color')
