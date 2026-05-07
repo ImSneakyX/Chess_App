4e08c1b9be45de1a4571d6c8fa68162ad95683f9
Paul Dulce <paul.dulce2107@gmail.com>
2026-05-07 16:27:02 +0200

    feat: Black and White exist now

--- chessboard/move.py
+++ chessboard/move.py
@@ -5,7 +5,7 @@ from chessboard.board import Board
 import numpy as np
 
 
-class Move(Board):
+class Move_White(Board):
     def __init__(self, boardstate, start_square, end_square): 
         Board.__init__(self)
         self.start = boardstate
@@ -20,9 +20,49 @@ class Move(Board):
 
     def get_legal_move_mask(self):
         mask = np.zeros((8, 8), dtype = 'bool')
-        moves = self.piece.get_legal_moves(self.start, self.start_square)
-        for row, col in moves: 
-            mask[(row, col)] = True 
+        if self.piece.color == 'w':
+            moves = self.piece.get_legal_moves(self.start, self.start_square)
+            for row, col in moves: 
+                mask[(row, col)] = True 
+        self.legal_move_mask = mask
+        return mask
+
+    def is_move_legal(self): 
+        if self.legal_move_mask[self.end_square] == True:
+            print(f'move is legal')
+        else:
+            print(f'move is not legal')
+
+
+    def move(self):
+        self.start1 = self.start.copy()
+        if self.legal_move_mask[self.end_square] == True:
+            self.start1[self.end_square] = self.piece
+            self.start1[self.start_square] = self.empty
+            self.pos_new = self.start1
+            self.display('name', self.pos_new)
+        else: self.pos_new = self.start1
+        return self.pos_new
+    
+class Move_Black(Board):
+    def __init__(self, boardstate, start_square, end_square): 
+        Board.__init__(self)
+        self.start = boardstate
+        self.start_square = start_square
+        self.end_square = end_square
+        self.piece = self.start[self.start_square]
+
+        self.legal_move_mask = None
+        self.get_legal_move_mask()
+        self.is_move_legal()
+        self.move()
+
+    def get_legal_move_mask(self):
+        mask = np.zeros((8, 8), dtype = 'bool')
+        if self.piece.color == 'b':
+            moves = self.piece.get_legal_moves(self.start, self.start_square)
+            for row, col in moves: 
+                mask[(row, col)] = True 
         self.legal_move_mask = mask
         return mask
 
@@ -48,7 +88,7 @@ class Move(Board):
 if __name__ == '__main__':
     brett = Board()
     brett.start_position()
-    brett1 = Move(brett.start, (7, 6), (5, 5))
+    brett1 = Move_White(brett.start, (7, 6), (5, 5))
     print(brett1.get_legal_move_mask())
 
 
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys 
-from chessboard.move import Move
+from chessboard.move import Move_White, Move_Black
 from chessboard.board import Board
 
 class Game: 
@@ -9,14 +9,14 @@ class Game:
         self.brett.chessboard_notation()
         self.position = self.brett.start
         self.start_game()
-        self.make_move()
+        self.make_move_white()
 
     def start_game(self):
         print(f'The game begins!')
         self.brett.display('name', self.brett.start)
     
-    def make_move(self):
-        start_square = input(f'Make a move! Select the square with the piece that you want to move: ')
+    def make_move_white(self):
+        start_square = input(f'White, make a move! Select the square with the piece that you want to move: ')
         end_square = input(f'and select the square you want to put the piece on: ')
         for row, i in enumerate(self.brett.notation):
             for col, j in enumerate(i):
@@ -26,10 +26,24 @@ class Game:
                     end_square = tuple((row, col))
 
 
-        x = Move(self.position, start_square, end_square)
+        x = Move_White(self.position, start_square, end_square)
         self.position = x.pos_new #wird wieder zu Brett umgewandelt
-        self.make_move() #ruft sich wieder sofort auf
+        self.make_move_black() #ruft die andere func auf
 
+    def make_move_black(self):
+        start_square = input(f'Black, make a move! Select the square with the piece that you want to move: ')
+        end_square = input(f'and select the square you want to put the piece on: ')
+        for row, i in enumerate(self.brett.notation):
+            for col, j in enumerate(i):
+                if  self.brett.notation[row, col] == start_square:
+                        start_square = tuple((row, col))
+                if  self.brett.notation[row, col] == end_square:
+                        end_square = tuple((row, col))
+
+
+        x = Move_Black(self.position, start_square, end_square)
+        self.position = x.pos_new #wird wieder zu Brett umgewandelt
+        self.make_move_white() #ruft wieder sofort die andere func uf
 
 
 
