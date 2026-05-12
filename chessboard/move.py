929be0dce7caf85ba336c9905359e0c5c00d6b37
Paul Dulce <paul.dulce2107@gmail.com>
2026-05-12 17:20:10 +0200

    fix: Fixed en passant

--- chessboard/move.py
+++ chessboard/move.py
@@ -115,7 +115,8 @@ class Move_White(Move):
 
             if isinstance(self.piece, Pawn) and self.start_square[0] == 6 and self.end_square[0] == 4:
                 self.start1[self.end_square] = self.pawn_w 
-                self.start1[self.end_square].two_steps()
+                self.pawn_w.two_steps()
+
 
 
             self.pos_new = self.start1
@@ -138,6 +139,10 @@ class Move_White(Move):
             self.moved_king = self.king.move_tracker(self.king_start, self.start_square)
             self.moved_rook_l = self.rook_l.move_tracker(self.rook_starts[0], self.start_square)
             self.moved_rook_r = self.rook_r.move_tracker(self.rook_starts[1], self.start_square)
+            for row, i in enumerate(self.start1):
+                for col, j in enumerate(i):
+                    if isinstance(self.start1[row, col], Pawn) and self.start1[row, col] != self.pawn_w: 
+                        self.start1[row, col].moved_two_steps = False
         return self.pos_new
 
 
@@ -217,7 +222,7 @@ class Move_Black(Move):
             
             if isinstance(self.piece, Pawn) and self.start_square[0] == 1 and self.end_square[0] == 3:
                 self.start1[self.end_square] = self.pawn_b
-                self.start1[self.end_square].two_steps()
+                self.pawn_b.two_steps()
 
             self.pos_new = self.start1
             self.visions(self.pos_new)
@@ -239,6 +244,10 @@ class Move_Black(Move):
             self.moved_king = self.king.move_tracker(self.king_start, self.start_square)
             self.moved_rook_l = self.rook_l.move_tracker(self.rook_starts[0], self.start_square)
             self.moved_rook_r = self.rook_r.move_tracker(self.rook_starts[1], self.start_square)
+            for row, i in enumerate(self.start1):
+                for col, j in enumerate(i):
+                    if isinstance(self.start1[row, col], Pawn) and self.start1[row, col] != self.pawn_b: 
+                        self.start1[row, col].moved_two_steps = False
         return self.pos_new
     
 
