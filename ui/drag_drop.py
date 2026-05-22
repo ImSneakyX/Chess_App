93e4c1cd7d1c657397c78a9019bd3951abf48086
Paul <paul.dulce2107@gmail.com>
2026-05-22 16:47:33 +0200

    feat: Trying to implement new drag and drop feature

---
+++ ui/drag_drop.py
@@ -0,0 +1,7 @@
+import sys
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QWidget, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton
+from PyQt5.QtGui import QIcon, QFont, QPixmap
+from PyQt5.QtCore import Qt
+
+class 
