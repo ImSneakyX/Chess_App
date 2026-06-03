fd353c6193b6a607151d55f4a40e0b0ab861c595
Paul Dulce <paul.dulce2107@gmail.com>
2026-06-03 15:34:10 +0200

    feat: added game engine and delted rules

---
+++ chessboard/game_engine.py
@@ -0,0 +1,27 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from chessboard.move import Move_White, Move_Black
+from chessboard.board import Board
+
+class Game_Engine:
+    def __init__(self):
+        self.white_to_move = True
+        self.brett = Board()
+        self.start_pos = self.brett.start_position()
+        self.position = self.brett.start_position()
+
+    def check_move(self, start_square, end_square):
+        if self.white_to_move == True:
+            x = Move_White(self.position, start_square, end_square, self.start_pos)
+
+        else:
+            x = Move_Black(self.position, start_square, end_square, self.start_pos)
+
+        if x.legal == True:
+            self.position = x.pos_new
+            self.white_to_move = not self.white_to_move
+            return True
+        return False
+
+        
