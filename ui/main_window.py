37b4de728cbde6945b4b21fcf735d7dbed76d252
Paul Dulce <paul.dulce2107@gmail.com>
2026-07-01 17:37:40 +0200

    feat: Button in launcher works now for computer game

--- ui/main_window.py
+++ ui/main_window.py
@@ -34,6 +34,7 @@ class Launcher(QMainWindow):
         layout.addWidget(button2)
         layout.addWidget(button3)
         button1.clicked.connect(self.game)
+        button2.clicked.connect(self.computer)
         central_widget.setLayout(layout)
 
     def game(self):
@@ -41,6 +42,13 @@ class Launcher(QMainWindow):
         self.window.launcher_signal.connect(lambda: self.show())
         self.window.show()
         self.hide()
+
+    def computer(self):
+        self.window = Computer_Game()
+        self.window.launcher_signal.connect(lambda: self.show())
+        self.window.show()
+        self.hide()
+
 
 class EngineWorker(QObject):
     finished = pyqtSignal(float)
@@ -185,6 +193,129 @@ class ChessGame(QMainWindow):
 
         
 
+
+class Computer_Game(QMainWindow):
+    launcher_signal = pyqtSignal()
+    def __init__(self):
+        self.arrows = []
+        super().__init__()
+                
+        self.resign_button_pressed = False
+        self.setWindowTitle('Schach')
+        self.setGeometry(350, 450, 1024, 683)
+        self.central_widget = QWidget()
+        self.central_widget.setStyleSheet(f'background-color: #28292b;')
+        self.setCentralWidget(self.central_widget)
+        self.setMinimumSize(600, 350)
+
+
+        self.GameController = GameController()
+
+        self.board_widget = ChessBoard(self.GameController.gameEngine, self)
+        self.board_widget.arrow_signal.connect(self.get_arrow_signal)
+        self.board_widget.delete_signal.connect(self.delete_arrows)
+        self.board_widget.move_signal.connect(self.update_evalbar)
+        self.board_widget.setGeometry(20, 20, self.width() // 2, 3 * self.height() // 4)
+
+        self.overlay = ArrowOverlay(self.arrows, self.board_widget)
+        self.overlay.setGeometry(0, 0, self.board_widget.width(), self.board_widget.height())
+
+        self.resign_button = QPushButton(self)
+        self.resign_button.setGeometry(40+ self.board_widget.width(), 40+ self.board_widget.height(), self.board_widget.width()//4, self.board_widget.width()//16)
+        self.resign_button.setText('RESIGN')
+        self.resign_button.setStyleSheet(f''' QPushButton {{border: none; font-family: Arial; font-weight: bold; font-size: 15px; color: white; background-color: #f54242;}}
+                                         QPushButton:hover {{background-color: #ff7a7a}}''')
+        self.resign_button.clicked.connect(self.resign)
+
+
+        self.eval_bar = EvalBar(self.board_widget.width() // 24, self.board_widget.height(), self)
+        self.eval_bar.move(7*self.width() // 8, 20)
+        self.eval_bar.resize(self.board_widget.width() // 24, self.board_widget.height())
+
+
+
+
+    def resign(self):
+
+        self.resign_button_pressed = True
+        width = 256
+        height = 130
+        self.confirmation = Confirmation(width, height, self)
+        self.confirmation.new_game_signal.connect(self.new_game)
+        self.confirmation.menu_signal.connect(self.back_to_menu)
+        self.confirmation.setGeometry(self.resign_button.x(), self.resign_button.y() - 150 , width, height)
+        self.confirmation.show()
+
+
+    
+    def back_to_menu(self):
+        self.launcher_signal.emit()
+        self.close()
+
+    def new_game(self):
+        self.GameController.gameEngine.position = copy.deepcopy(self.GameController.position)
+        self.board_widget.board_reset()
+
+
+
+
+    
+    
+
+
+
+
+
+    def analysis_finished(self, value):
+
+        print(self.GameController.ultimate.calculations)
+        self.GameController.ultimate.calculations = 0
+        self.eval_bar.setEval(value)
+
+
+
+    def update_evalbar(self, position):
+
+        self.thread = QThread()
+        self.worker = EngineWorker(self.GameController.ultimate, copy.deepcopy(position))
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.analysis_finished)
+        self.worker.finished.connect(self.thread.quit)
+        self.thread.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+        self.thread.start()
+
+
+
+    def get_arrow_signal(self, arrows):
+        self.arrows.append(arrows[0])
+        self.overlay.update()
+
+    def delete_arrows(self):
+        self.arrows.clear()
+        self.overlay.update()
+
+
+    def resizeEvent(self, e):
+
+        super().resizeEvent(e)
+
+        size = min(self.width() // 2, 3 * self.height() // 4)
+        self.board_widget.resize(size, size)
+
+        self.overlay.resize(self.board_widget.size())
+        self.overlay.show()
+
+        self.resign_button.setGeometry(40+ self.board_widget.width(), 40+ self.board_widget.height(), self.board_widget.width()//4, self.board_widget.width()//16)
+
+
+        self.eval_bar.resize(self.board_widget.width() // 24, self.board_widget.height())
+
+        if self.resign_button_pressed == True:
+
+            self.confirmation.close()
+            self.resign_button_pressed = False
 
 
 
