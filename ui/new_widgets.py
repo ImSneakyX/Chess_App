125b3bc5bc517e8bbb1eff45ed76c300a614b08e
Paul Dulce <paul.dulce2107@gmail.com>
2026-08-14 19:24:35 +0200

    feat: New Widget

--- ui/new_widgets.py
+++ ui/new_widgets.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication
 from PyQt5.QtGui import QPainter, QBrush, QColor, QPen
-from PyQt5.QtCore import QRectF
+from PyQt5.QtCore import QRectF, pyqtProperty, QPropertyAnimation, Qt, QPointF
 import sys
 
 class ToggleSwitch(QWidget):
@@ -9,23 +9,53 @@ class ToggleSwitch(QWidget):
         super().__init__(parent)
         self.setFixedSize(60, 32)
 
+        self._checked = False
+        self._x_circle = 3
+        self.anim = QPropertyAnimation(self, b'x_circle')
+        self.anim.setDuration(180)
+
+    def get_x(self):
+        return self._x_circle
+    
+    def set_x(self, x):
+
+        self._x_circle = x
+        self.update()
+
+    x_circle = pyqtProperty(float, get_x, set_x)
 
     
+    def mousePressEvent(self, event):
 
+        self._checked = not self._checked
+        self.anim.stop()
 
+        self.anim.setStartValue(self._x_circle)
+
+        if self._checked:
+            self.anim.setEndValue(31)
+        else: 
+            self.anim.setEndValue(3)
+
+        self.anim.start()
+
+        
     def paintEvent(self, event):
 
         p = QPainter(self)
         p.setRenderHint(QPainter.RenderHint.Antialiasing)
-        color  = QColor("#000000")
+        if self._checked:
+            bg = '#00c853'
+        else: 
+            bg = '#4a4a4a'
 
-        brush = QBrush(color)
-        pen = QPen(color)
-
-        p.setPen(pen)
-        #p.setBrush(brush)
-        rec = QRectF(0, 0, self.width() - 2, self.height() - 2)
-        p.drawRoundedRect(rec, 50, 50)
+        brush = QBrush(QColor(bg))
+        p.setPen(Qt.PenStyle.NoPen)
+        p.setBrush(brush)
+        rec = QRectF(0, 0, self.width(), self.height())
+        p.drawRoundedRect(rec, 16, 16)
+        p.setBrush(QBrush(QColor('white')))
+        p.drawEllipse(QPointF(self._x_circle + 13, 16), 13, 13)
 
 
 class MainWindow(QWidget):
